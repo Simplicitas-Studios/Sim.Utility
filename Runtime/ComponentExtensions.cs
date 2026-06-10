aa846e580f0c01875fed2fd3de7acd489d3c8139
pasdaa13 <pasdaa13@gmail.com>
2026-06-10 17:27:22 +0200

    Add ComponentExtensions to get ScenePath

---
+++ Runtime/ComponentExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Sim.Utility
+{
+    public static class ComponentExtensions
+    {
+        public static string GetPath(this Component component)
+        {
+            return string.Join("/",
+                component.GetComponentsInParent<Transform>(true)
+                    .Select(t => t.name)
+                    .Reverse());
+        }
+    }
+}
